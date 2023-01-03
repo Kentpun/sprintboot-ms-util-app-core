d0fdbfab13883557d3997edd09681fa595735650
wayu <wayu@deloitte.com.hk>
2023-01-03 10:59:32 +0800

    Amend imports

--- src/main/java/com/deloitte/core/Test.java
+++
@@ -1,4 +0,0 @@
-package com.deloitte.core;
-
-public class Test {
-}
--- src/main/java/com/deloitte/core/entity/AuditEntity.java
+++ src/main/java/com/deloitte/core/entity/AuditEntity.java
@@ -1,7 +1,5 @@
 package com.deloitte.core.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,11 +7,10 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.Version;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 import java.io.Serializable;
-import java.time.Instant;
 import java.util.Date;
 
 
