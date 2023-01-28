813e2c948e472a6f72c6de0ad8ecd5eca27c9a73
robjiang <3nmw^!E!MD>
2023-01-28 17:22:53 +0800

    add LuhnAlgorithm

--- src/main/java/com/deloitte/core/utils/LuhnAlgorithmUtil.java
+++ src/main/java/com/deloitte/core/utils/LuhnAlgorithmUtil.java
@@ -1,5 +1,7 @@
 package com.deloitte.core.utils;
 
+import java.util.Random;
+
 public class LuhnAlgorithmUtil {
     // Returns true if given
     // card number is valid
@@ -23,4 +25,50 @@ public class LuhnAlgorithmUtil {
         }
         return (nSum % 10 == 0);
     }
+
+    /**
+     * 信用卡中间6位加密，生成新号码
+     *
+     * @param cardNo 信用卡号
+     * @return 生成的新号码
+     */
+    public static String getNewNum(String cardNo) {
+        int nDigits = cardNo.length();
+        // 校验位
+        int lastBitNum = cardNo.charAt(nDigits - 1) - '0';
+        // 前6位和后4位不变
+        String preSixBitNum = cardNo.substring(0, 6);
+        String lastForBitNum = cardNo.substring(12);
+        // 中间6位进行加密
+        String midSixBitNum = cardNo.substring(6, 12);
+
+        // 随机生成5位数字，第6位进行补位满足luhn算法
+        String randomNum = String.valueOf((int)((Math.random()*9 + 1) * 10000));
+        int sumNum = 0;
+        for (int i = 0; i < randomNum.length(); i++) {
+            int bitNum = randomNum.charAt(i) - '0';
+            // 第1、3、5位需乘以2，每一位分别求和+第2、4位
+            if (i % 2 == 0) {
+                int tempNum = bitNum * 2;
+                sumNum = sumNum + tempNum / 10;
+                sumNum = sumNum + tempNum % 10;
+            } else {
+                sumNum += bitNum;
+            }
+        }
+
+        // 加密第6位数字=(50+校验位-sumNum)%10
+        int encryptSixBitNum = (50 + lastBitNum - sumNum) % 10;
+        return preSixBitNum.concat(randomNum).concat(String.valueOf(encryptSixBitNum)).concat(lastForBitNum);
+    }
+
+//    public static  void main(String[] args) {
+//        String cardnum = "9453677629008564"; // 9453677629008564   945367 376685 8564
+//        String luhnNum = getNewNum(cardnum);
+//        System.out.println(luhnNum);
+//        boolean result = checkLuhn(luhnNum);
+//        boolean result1 = checkLuhn(cardnum);
+//        System.out.println(result1);
+//        System.out.println(result);
+//    }
 }
