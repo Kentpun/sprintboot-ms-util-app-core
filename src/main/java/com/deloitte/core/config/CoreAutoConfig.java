45d8cc8415fbfee264c53a13d6390dedf26bea87
Wai Tsang <wtsang@deloitte.com.hk>
2023-02-08 10:19:25 +0800

    Add config prop for api uri management

--- src/main/java/com/deloitte/core/config/CoreAutoConfig.java
+++ src/main/java/com/deloitte/core/config/CoreAutoConfig.java
@@ -1,14 +1,15 @@
 package com.deloitte.core.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import lombok.RequiredArgsConstructor;
+import com.deloitte.core.config.prop.AppApiProp;
 
 @Configuration(proxyBeanMethods = false)
-@RequiredArgsConstructor
 @Import({//
     WebClientConfig.class//
 })
+@EnableConfigurationProperties(AppApiProp.class)
 public class CoreAutoConfig {
 
 }
